da16488801238cabcc0d0d904efed5afbb83316f
junjia.njj <junjia.njj@alibaba-inc.com>
2015-11-14 23:27:55 +0800

    "For Deployment Sat Nov 14 2015 23:27:55 GMT+0800 (CST)"

--- bin.js
+++ bin.js
@@ -3,78 +3,11 @@
 'use strict'
 
 var child_process = require('child_process')
+var child1 = child_process.fork(__dirname + '/deploy.js', [process.argv[2], 'articles'])
+var child2 = child_process.fork(__dirname + '/deploy.js', [process.argv[2], 'master'])
+child1.on('exit', function () {
+  // console.log(arguments, this);
+})
+child2.on('exit', function () {
 
-class Deployer {
-  // branch 分支的名称
-  // dirname 站点（blog）的路径在哪里
-  constructor() {
-    if (!dirname) var dirname = __dirname
-    this.root = dirname
-  }
-
-  start() {
-    // console.log(process.argv)
-    if (process.argv.length > 3) {
-      console.log('COMMAND error! The comment must be wrapped by ""');
-      console.log('e.g. $ node deploy.js "The comment you want to input."');
-      process.exit(0)
-    }
-    var comment = process.argv[2]
-    var A = child_process.fork(this['exec-sh'](this['push-github-master'](comment)))
-    var B = child_process.fork(this['exec-sh'](this['push-github-articles'](comment)))
-  }
-
-  'exec-sh' (sh) {
-    sh.every(function(cwd) {
-      if (typeof cwd === 'string' || cwd instanceof String) {
-        cwd = cwd.split(' ')
-      }
-      console.log(`-> ${cwd.join(' ')}`);
-      var rlt = spawnSync(cwd.shift(), cwd)
-      if (rlt.status === null) {
-        console.log(`command cannot found: ${rlt.args[0]}`)
-        process.exit(1)
-      }
-      if (rlt.status === 0) {
-        // var stdout = rlt.stdout.toString()
-        // console.log(stdout)
-        return true
-      } else {
-        if (rlt.stderr) console.log(rlt.stderr.toString())
-        if (rlt.stdout) console.log(rlt.stdout.toString())
-        return false
-      }
-    })
-  }
-
-  'push-github-articles' (comment) {
-    if (!comment) var comment = "For Deployment " + (new Date())
-    var branch = 'articles'
-    return [
-      `cd ${this.root}`,
-      // `git checkout ${branch}`,
-      `git add _articles static deploy.js README.org _draft favicon.ico`, // TODO: here will be contain deploy.js
-      ['git', 'commit', '-m', `"${comment}"`],
-      `git push origin ${branch}:${branch}`
-    ]
-  }
-
-  'push-github-master' (comment) {
-    if (!comment) var comment = "For Deployment " + (new Date())
-    var branch = 'master'
-    return [
-      `cd ${this.root}/public`,
-      // `git checkout ${branch}`,
-      `git add .`, ['git', 'commit', '-m', `"${comment}"`],
-      `git push origin ${branch}:${branch}`
-    ]
-  }
-
-}
-
-(function() {
-  'use strict'
-  // console.log = function () {}
-  var d = new Deployer
-  d.start()
-})()
+})
