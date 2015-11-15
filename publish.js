9939467c5a33cb1a7db3beea451cb89ff290223c
junjia.njj <junjia.njj@alibaba-inc.com>
2015-11-15 22:03:22 +0800

    Publish at Sun Nov 15 2015 22:03:22 GMT+0800 (CST)

--- publish.js
+++ publish.js
@@ -1,5 +1,48 @@
 'use strict'
 
+function Publish(body, content) {
+  var btnPublish, editorContainer, ws
+  ws = createWebsocket()
+  ws.onopen = function(event) {
+
+  }
+  ws.onclose = function() {
+    body.innerHTML = '<h1>WebSocket Disconnected!</h1>'
+  }
+  ws.onmessage = function(message) {
+    var data = JSON.parse(message.data)
+    require(['oenkace'], function(editor) {
+      editor.insert(data.stdout)
+      editorContainer.style.position = 'absolute'
+    })
+  }
+
+  loadRequirejs(body)
+  editorContainer = createEditorContainer(body)
+  btnPublish = addPublishBtn(body, content, ws)
+
+}
+
+function createWebsocket() {
+  var ws, oldSend
+  ws = new WebSocket('ws://' + window.location.hostname + ':3001')
+  oldSend = ws.send
+  ws.send = function(message) {
+    if (typeof message === 'string' || message instanceof String) {} else {
+      message = JSON.stringify(message)
+    }
+    return oldSend.apply(ws, [message])
+  }
+  return ws
+}
+
+function createEditorContainer (body) {
+  var div = document.createElement('div')
+  div.id = 'editor'
+  body.appendChild(div)
+  return div
+}
+
 function addPublishBtn(body, content, ws) {
   var btn = document.createElement('button')
   btn.innerText = 'Publish'
@@ -12,17 +55,45 @@ function addPublishBtn(body, content, ws) {
     ws.send(message)
   }
   body.appendChild(btn)
+  return btn
+}
+
+function loadRequirejs(body) {
+  var s = document.createElement('script')
+  s.src = '/requirejs/require.js'
+  s.type = 'text\/javascript'
+  s.onload = function(e) {
+    require.config({
+      // baseUrl: '/ace/lib/ace',
+      paths: {
+        ace: "/ace/lib/ace"
+      }
+    })
+
+    define('oenkace', ['ace/ace'], function(ace, langtools) {
+      var editor = ace.edit('editor')
+      editor.setOptions({
+        readOnly: true,
+        highlightActiveLine: false,
+        highlightGutterLine: false
+      })
+      editor.$blockScrolling = Infinity
+      editor.setTheme("ace/theme/ambiance")
+      editor.session.setMode("ace/mode/sh")
+      return editor
+    })
+
+  }
+  body.appendChild(s)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-
 
   var pathname = window.location.pathname
   var body = document.getElementsByTagName('body')[0]
   var content = document.getElementById('content')
   var meta = getMetaInfo(true)
   var isHome = false
-  var ws, oldSend
 
   if (window.location.pathname === '/') isHome = true
 
@@ -32,35 +103,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   if (!isHome) {
-    ws = new WebSocket('ws://' + window.location.hostname + ':3001')
-    oldSend = ws.send
-
-    /**
-    * Send function
-    */
-    ws.send = function(message) {
-      if (typeof message === 'string' || message instanceof String) {
-
-      } else {
-        message = JSON.stringify(message)
-      }
-      return oldSend.apply(ws, [message])
-    }
-
-    ws.onopen = function(event) {
-
-    }
-
-    ws.onclose = function() {
-      body.innerHTML = '<h1>WebSocket Disconnected!</h1>'
-    }
-
-    ws.onmessage = function(message) {
-      var data = JSON.parse(message.data)
-      console.log(data.stdout)
-    }
-
-    addPublishBtn(body, content, ws)
+    Publish(body, content)
   }
 
 })
