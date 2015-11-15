b519298957539aa0329a35380a10e7e1dfb2c870
junjia.njj <junjia.njj@alibaba-inc.com>
2015-11-15 11:44:09 +0800

    Publish at Sun Nov 15 2015 11:44:09 GMT+0800 (CST)

--- deploy.js
+++ deploy.js
@@ -36,7 +36,7 @@ class Deployer {
       if (typeof cwd === 'string' || cwd instanceof String) {
         cwd = cwd.split(' ')
       }
-      console.log(util.inspect(`$ ${cwd.join(' ')} [process:${process.pid}]`, { depth: null }));
+      util.inspect(`$ ${cwd.join(' ')} [process:${process.pid}]`, { depth: null })
       var rlt = spawnSync(cwd.shift(), cwd)
       if (rlt.status === null) {
         console.log(`command cannot found: ${rlt.args[0]}`)
