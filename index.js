c9e120dbd32efa77b8a54ac090fba37974b9728c
junjia.njj <junjia.njj@alibaba-inc.com>
2015-11-15 12:15:44 +0800

    Publish at Sun Nov 15 2015 12:15:43 GMT+0800 (CST)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var router = require('koa-router')()
  * 发布
  */
 router.get('/publish', function() {
-  var child1 = child_process.fork(__dirname + '/deploy.js', ['articles', '_articles _draft static README.org  favicon.ico bin.js deploy.js', process.argv[2]])
+  var child1 = child_process.fork(__dirname + '/deploy.js', ['articles', '_articles _draft static README.org favicon.ico bin.js deploy.js', process.argv[2]])
   var child2 = child_process.fork(__dirname + '/deploy.js', ['master', '.', process.argv[2]])
   this.body = 'success'
 })
