830dbd9c088ed7e04c7b4d938654580edbc35ff8
junjia.njj <junjia.njj@alibaba-inc.com>
2015-11-15 10:06:53 +0800

    "For Deployment Sun Nov 15 2015 10:06:53 GMT+0800 (CST)"

--- bin.js
+++ bin.js
@@ -3,11 +3,19 @@
 'use strict'
 
 var child_process = require('child_process')
-var child1 = child_process.fork(__dirname + '/deploy.js', [process.argv[2], 'articles'])
-var child2 = child_process.fork(__dirname + '/deploy.js', [process.argv[2], 'master'])
-child1.on('exit', function () {
-  // console.log(arguments, this);
-})
-child2.on('exit', function () {
+var child1, child2
 
-})
+if (process.argv.length > 3) {
+  console.log('COMMAND error! The comment must be wrapped by ""');
+  console.log('e.g. $ node deploy.js "The comment you want to input."');
+  process.exit(0)
+} else {
+  child1 = child_process.fork(__dirname + '/deploy.js', [process.argv[2], 'articles'])
+  child2 = child_process.fork(__dirname + '/deploy.js', [process.argv[2], 'master'])
+  child1.on('exit', function() {
+    // console.log(arguments, this);
+  })
+  child2.on('exit', function() {
+
+  })
+}
