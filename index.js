d4df1fe9584ba8cc02a0ac651db748077f29dd11
junjia.njj <junjia.njj@alibaba-inc.com>
2015-11-15 22:57:47 +0800

    Publish at Sun Nov 15 2015 22:57:47 GMT+0800 (CST)

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ var wss = new WebSocketServer({
   port: 3001
 })
 
-/*
- * 发布
- */
-router.get('/publish', function() {
-  var child1 = child_process.fork(__dirname + '/deploy.js', ['articles', '_articles _draft static README.org favicon.ico bin.js deploy.js', process.argv[2]])
-  var child2 = child_process.fork(__dirname + '/deploy.js', ['master', '.', process.argv[2]])
-  this.body = '<p style="text-align: center; font-size: 4em;">The server is publish...<p>'
-})
-
 app.use(function*(next) {
     // console.log(this)
     yield next
@@ -55,6 +46,7 @@ wss.on('connection', function connection(ws) {
       child1.on('message', function (message) {
         ws.send(JSON.stringify(message))
       }).on('SIGINT', function () {
+        console.log('==============')
         ws.send(`Process:${this.pid} published ${this.spawnargs[2]} over.`)
       })
       child2.on('message', function (message) {
