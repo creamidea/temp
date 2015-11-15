c2f9520a4475157a6693b98132a41323663f4eb9
junjia.njj <junjia.njj@alibaba-inc.com>
2015-11-15 12:06:37 +0800

    Publish at Sun Nov 15 2015 12:06:37 GMT+0800 (CST)

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var child_process = require('child_process')
+
+var app = require('koa')()
+var router = require('koa-router')()
+
+/*
+ * 发布
+ */
+router.get('/publish', function() {
+  child1 = child_process.fork(__dirname + '/deploy.js', ['articles', '_articles _draft static README.org  favicon.ico bin.js deploy.js', process.argv[2]])
+  child2 = child_process.fork(__dirname + '/deploy.js', ['master', '.', process.argv[2]])
+  this.body = 'success'
+})
+
+app.use(require('koa-static')(__dirname + '/public'))
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3000)
+console.log('listening on port 3000')
